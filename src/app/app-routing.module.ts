b50fc07b3e8c8bdbbf3e11c208a465380141479d
Laura Rintoul <laura.rintoul@accolitedigital.com>
2022-12-21 14:19:49 +0000

    deleted unused imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AlertsComponent } from './components/PageComponents/alerts/alerts.component';
-import { HomeComponent } from './components/home/home.component';
-import { MilestonesComponent } from './components/milestones/milestones.component';
-import { PermissionsComponent } from './components/permissions/permissions.component';
-import { ReportingComponent } from './components/reporting/reporting.component';
-import { TfrsComponent } from './components/tfrs/tfrs.component';
-import { TfrComponent } from './components/tfr/tfr.component';
 import { RoutesService } from './routes.service';
 
 @NgModule({
