0ab2e2bfb2a216130506c1a9fb353382154c5b2a
Laura Rintoul <laura.rintoul@accolitedigital.com>
2022-12-22 15:53:29 +0000

    made existing tfrs openable

--- src/app/components/PageComponents/tfr-creation/tfr-creation.component.ts
+++ src/app/components/PageComponents/tfr-creation/tfr-creation.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { ChipEmitterService } from 'src/app/services/chip-emitter.service';
 import { MilestoneManagerService } from 'src/app/services/milestone-manager.service';
+import { CoreMaterialModule } from 'src/app/core-modules/core-material/core-material.module';
 
 @Component({
   selector: 'app-tfr-creation',
@@ -9,6 +10,7 @@ import { MilestoneManagerService } from 'src/app/services/milestone-manager.serv
   styleUrls: ['./tfr-creation.component.scss'],
 })
 export class TfrCreationComponent implements OnInit {
+  tfrid: number = 76839;
   milestoneForm = new FormGroup({
     name: new FormControl(''),
     description: new FormControl(''),
@@ -35,12 +37,30 @@ export class TfrCreationComponent implements OnInit {
       this.milestones = this.milestoneManagerService.getMilestones();
       console.log(this.milestones);
       this.selectedMilestone = this.milestoneManagerService.getSelected();
+      this.milestoneForm.setValue(this.selectedMilestone);
     },
   };
   ngOnInit(): void {
     this.chipEmitterService.ToRemove.subscribe(this.removalObserver);
     this.chipEmitterService.ToSelect.subscribe(this.selectionObserver);
     this.milestoneManagerService.Update.subscribe(this.updateObserver);
-    this.milestoneManagerService.add({ name: 'hello' });
+    this.milestoneManagerService.add({
+      name: 'hello',
+      description: 'trial',
+      startDate: '5thNov',
+      endDate: '6thNov',
+    });
+    console.log(this.milestoneForm.value);
+  }
+  getFormMilestone() {
+    return this.milestoneForm.value;
+  }
+  addNew() {
+    this.milestoneManagerService.add({
+      name: 'example',
+      description: '',
+      startDate: '',
+      endDate: '',
+    });
   }
 }
--- src/app/core-modules/core-material/core-material.module.ts
+++ src/app/core-modules/core-material/core-material.module.ts
@@ -10,6 +10,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
