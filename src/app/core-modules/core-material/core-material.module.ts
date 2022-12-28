9d0f0f0fe01aceb96a8241b48cf29d1a96f7497c
Laura Rintoul <laura.rintoul@accolitedigital.com>
2022-12-28 17:39:11 +0000

    changing things to materials angular

--- src/app/core-modules/core-material/core-material.module.ts
+++ src/app/core-modules/core-material/core-material.module.ts
@@ -2,41 +2,36 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from '@angular/material/icon';
-
 import { MatButtonModule } from '@angular/material/button';
-
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+const modules = [
+  BrowserModule,
+  FormsModule,
+  FlexLayoutModule,
+  BrowserAnimationsModule,
+  MatIconModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  CommonModule,
+  MatCardModule,
+  MatChipsModule,
+  MatInputModule,
+  MatFormFieldModule,
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    FlexLayoutModule,
-    BrowserAnimationsModule,
-    MatIconModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    CommonModule,
-    MatCardModule,
-    MatChipsModule,
-  ],
-  exports: [
-    FlexLayoutModule,
-    MatIconModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatCardModule,
-    MatChipsModule,
-  ],
+  imports: [...modules],
+  exports: [...modules],
 })
 export class CoreMaterialModule {}
